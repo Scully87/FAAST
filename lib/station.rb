51c556044dbc406ac98ba00f4f76b724ae5d809d
Scully87 <Scully87.ts@googlemail.com>
2014-09-20 17:37:28 +0100

    Added station spec

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,32 @@
 class Station
 
+	def initialize
+		@passengers = []
+		@train = []
+		@station = []
+	end
+
+	def passenger_count
+		@passengers.count
+	end
+
+	def touch_in(passenger)
+		@passengers << passenger
+	end
+
+	def touch_out(passenger)
+		@passengers.delete(passenger)
+	end
+
+	def train_count
+		@train.count
+	end
+
+	def train_arrive(train)
+		@train << train
+	end
+
+	def train_depart(train)
+		@train.delete(train)
+	end
 end
---
+++ spec/station_spec.rb
@@ -0,0 +1,38 @@
+require 'station'
+require 'passenger'
+require 'carriage'
+require 'train'
+
+describe Station do 
+	
+	let(:station) { Station.new }
+	let(:passenger) { Passenger.new }
+	let(:carriage) { Carriage.new }
+	let(:train) { Train.new }
+
+
+	
+	it "should allow passengers to touch in" do
+		expect(station.passenger_count).to eq(0)
+		station.touch_in(passenger)
+		expect(station.passenger_count).to eq(1)
+	end
+
+	it "should allow passengers to touch out" do
+		station.touch_in(passenger)
+		station.touch_out(passenger)
+		expect(station.passenger_count).to eq(0)
+	end
+
+	it "should allow trains to arrive" do
+		expect(station.train_count).to eq(0)
+		station.train_arrive(train)
+		expect(station.train_count).to eq(1)
+	end
+
+	it "should allow trains to depart" do
+		station.train_arrive(train)
+		station.train_depart(train)
+		expect(station.train_count).to eq(0)
+	end
+end

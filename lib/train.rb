c92283f1a3af36400c56c2f44dd9e1ff155652cb
Scully87 <Scully87.ts@googlemail.com>
2014-09-21 20:42:05 +0100

    added journey from station to station

--- lib/train.rb
+++ lib/train.rb
@@ -3,10 +3,16 @@ require 'station'
 class Train
 
 	def initialize
+			@train = []
 			@carriages = 8.times { Carriage.new }
 	end
 
 	def carriage_length
 			@carriages
 	end
+
+	def journey(station_from=nil, station_to)
+			station_from.train_depart(self) if station_from !=nil
+			station_to.train_arrive(self)	
+	end	
 end	
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -8,4 +8,13 @@ describe Train do
 		expect(train.carriage_length).to eq(8)
 	end
 
+	it "can travel from station to station" do
+		station_1, station_2 = Station.new, Station.new
+		train.journey(station_1)
+		expect(station_1.train_count).to eq(1)
+		expect(station_2.train_count).to eq(0)
+		train.journey(station_1, station_2)
+		expect(station_1.train_count).to eq(0)
+		expect(station_2.train_count).to eq(1)
+	end
 end
